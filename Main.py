8c755190f9d9b085bb561d1ae65ecc8200ce2cf1
MrSunshine26 <osandoreynolds@hotmail.com>
2024-11-05 19:10:31 +0000

    Code and CSV change
    Renamed the CSV in line with my code
    Added the last of the code

--- Main.py
+++ Main.py
@@ -13,3 +13,17 @@ def whois_check(domain):
 
 def process_csv(input_file, output_file):
     with open(input_file, mode='r') as infile, open(output_file, mode='w', newline='') as outfile:
+        reader = csv.reader(infile)
+        writer = csv.writer(outfile)
+        writer.writerow(['Domain', 'Creation Date', 'Registrar'])
+
+        next(reader, None) #Skip the header
+        for row in reader:
+            domain = defang(row[0]0)
+            creation_date, registrar = whois_check(domain)
+            writer.writerow([domain, creation_date, registrar])
+
+if __name__ == "__main__":
+    input_csv = 'test_domains.csv'
+    output_csv = 'whois_output.csv'
+    process_csv(input_csv, output_csv)
